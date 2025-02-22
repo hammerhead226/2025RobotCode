17eca095734b68bab36ba7db05a3e9134be54a82
ArnavGuptaOfficial <arnavgupta045@gmail.com>
2025-02-22 13:41:40 -0500

    All Autons Now Working
    All of the autons are now working in the sim under 15 seconds. 3 coral autons.

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -5,12 +5,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "2025RobotCode";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 292;
-  public static final String GIT_SHA = "90c3495537eec29fe95f4eb62a233fa3d5c77ba8";
-  public static final String GIT_DATE = "2025-02-22 12:49:05 EST";
+  public static final int GIT_REVISION = 293;
+  public static final String GIT_SHA = "15385116b331fe061fe387606814fec4b4630749";
+  public static final String GIT_DATE = "2025-02-22 13:14:43 EST";
   public static final String GIT_BRANCH = "fixingAutoChooser-2-22-2025";
-  public static final String BUILD_DATE = "2025-02-22 13:13:27 EST";
-  public static final long BUILD_UNIX_TIME = 1740248007543L;
+  public static final String BUILD_DATE = "2025-02-22 13:38:19 EST";
+  public static final long BUILD_UNIX_TIME = 1740249499285L;
   public static final int DIRTY = 128;
 
   private BuildConstants() {}
