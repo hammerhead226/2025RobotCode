15385116b331fe061fe387606814fec4b4630749
ArnavGuptaOfficial <arnavgupta045@gmail.com>
2025-02-22 13:14:43 -0500

    Got BlueRight to Work
    BlueRIght now Works

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -5,12 +5,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "2025RobotCode";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 290;
-  public static final String GIT_SHA = "e8c197b9164dee013b29ac32d6e1842593b9b949";
-  public static final String GIT_DATE = "2025-02-22 11:49:35 EST";
+  public static final int GIT_REVISION = 292;
+  public static final String GIT_SHA = "90c3495537eec29fe95f4eb62a233fa3d5c77ba8";
+  public static final String GIT_DATE = "2025-02-22 12:49:05 EST";
   public static final String GIT_BRANCH = "fixingAutoChooser-2-22-2025";
-  public static final String BUILD_DATE = "2025-02-22 12:46:51 EST";
-  public static final long BUILD_UNIX_TIME = 1740246411785L;
+  public static final String BUILD_DATE = "2025-02-22 13:13:27 EST";
+  public static final long BUILD_UNIX_TIME = 1740248007543L;
   public static final int DIRTY = 128;
 
   private BuildConstants() {}
--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -367,10 +367,10 @@ public class RobotContainer {
     autos.addOption(
         "AutoTestBottom", AutoBuilder.buildAuto("Bottom-R3b(L4)-F1-R2a(L4)-F1-R2b(L4)"));
 
-    autoChooser.addOption(
-      "Drive Wheel Radius Characterization", DriveCommands.wheelRadiusCharacterization(drive));
-     autoChooser.addOption(
-         "Drive Simple FF Characterization", DriveCommands.feedforwardCharacterization(drive));
+    // autoChooser.addOption(
+    //  "Drive Wheel Radius Characterization", DriveCommands.wheelRadiusCharacterization(drive));
+    // autoChooser.addOption(
+    //  "Drive Simple FF Characterization", DriveCommands.feedforwardCharacterization(drive));
     //  autoChooser.addOption(
     //      "Drive SysId (Quasistatic Forward)",
     //      drive.sysIdQuasistatic(SysIdRoutine.Direction.kForward));
