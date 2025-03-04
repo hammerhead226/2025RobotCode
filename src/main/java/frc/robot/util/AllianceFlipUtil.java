445e30301d444df9f3bb97cb8bb6383bb5aa184b
devinisaperson <177495008+devinisaperson@users.noreply.github.com>
2025-03-04 18:24:22 -0500

    Update AllianceFlipUtil.java

--- src/main/java/frc/robot/util/AllianceFlipUtil.java
+++ src/main/java/frc/robot/util/AllianceFlipUtil.java
@@ -1,12 +1,17 @@
+<<<<<<< Updated upstream
 package frc.robot.util;
 
 // Copyright (c) 2024 FRC 6328
+=======
+// Copyright (c) 2025 FRC 6328
+>>>>>>> Stashed changes
 // http://github.com/Mechanical-Advantage
 //
 // Use of this source code is governed by an MIT-style
 // license that can be found in the LICENSE file at
 // the root directory of this project.
 
+<<<<<<< Updated upstream
 import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.geometry.Translation2d;
@@ -67,3 +72,75 @@ public class AllianceFlipUtil {
         && DriverStation.getAlliance().get() == Alliance.Red;
   }
 }
+=======
+
+
+import edu.wpi.first.math.geometry.*;
+import edu.wpi.first.wpilibj.DriverStation;
+import frc.robot.constants.FieldConstants;
+
+
+public class AllianceFlipUtil {
+  public static double applyX(double x) {
+    return shouldFlip() ? FieldConstants.fieldLength - x : x;
+  }
+
+  public static double applyY(double y) {
+    return shouldFlip() ? FieldConstants.fieldWidth - y : y;
+  }
+
+  public static Translation2d apply(Translation2d translation) {
+    return new Translation2d(applyX(translation.getX()), applyY(translation.getY()));
+  }
+
+  public static Rotation2d apply(Rotation2d rotation) {
+    return shouldFlip() ? rotation.rotateBy(Rotation2d.kPi) : rotation;
+  }
+
+  public static Pose2d apply(Pose2d pose) {
+    return shouldFlip()
+        ? new Pose2d(apply(pose.getTranslation()), apply(pose.getRotation()))
+        : pose;
+  }
+
+  public static Translation3d apply(Translation3d translation) {
+    return new Translation3d(
+        applyX(translation.getX()), applyY(translation.getY()), translation.getZ());
+  }
+
+  public static Rotation3d apply(Rotation3d rotation) {
+    return shouldFlip() ? rotation.rotateBy(new Rotation3d(0.0, 0.0, Math.PI)) : rotation;
+  }
+
+  public static Pose3d apply(Pose3d pose) {
+    return new Pose3d(apply(pose.getTranslation()), apply(pose.getRotation()));
+  }
+
+  // public static VehicleState apply(VehicleState state) {
+  //   return shouldFlip()
+  //       ? disableHAL.newBuilder()
+  //           .setX(applyX(state.getX()))
+  //           .setY(applyY(state.getY()))
+  //           .setTheta(apply(Rotation2d.fromRadians(state.getTheta())).getRadians())
+  //           .setVx(-state.getVx())
+  //           .setVy(-state.getVy())
+  //           .setOmega(state.getOmega())
+  //           .addAllModuleForces(
+  //               state.getModuleForcesList().stream()
+  //                   .map(
+  //                       forces ->
+  //                           ModuleForce.newBuilder()
+  //                               .setFx(-forces.getFx())
+  //                               .setFy(-forces.getFy())
+  //                               .build())
+  //                   .toList())
+  //           .build()
+  //       : state;
+  // }
+
+  public static boolean shouldFlip() {
+    return DriverStation.getAlliance().isPresent()
+        && DriverStation.getAlliance().get() == DriverStation.Alliance.Red;
+  }
+}
+>>>>>>> Stashed changes
