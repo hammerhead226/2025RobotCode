18487ccf4e4be6f9c34bf246d1408b8026086e5d
David Chen <dychen2523@gmail.com>
2025-03-31 22:08:02 -0400

    0.4m later elevaotr up for auto align

--- src/main/java/frc/robot/subsystems/drive/Drive.java
+++ src/main/java/frc/robot/subsystems/drive/Drive.java
@@ -630,7 +630,7 @@ public class Drive extends SubsystemBase {
     return getPose()
             .getTranslation()
             .getDistance(AllianceFlipUtil.apply(FieldConstants.Reef.center))
-        <= 2.5;
+        <= 2.1;
     // <= 0.5;
   }
 
