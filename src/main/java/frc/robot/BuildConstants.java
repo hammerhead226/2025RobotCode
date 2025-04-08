76164bc2dc227a63d895b8c2ae9a9b6da1b33587
ArnavGuptaOfficial <arnavgupta045@gmail.com>
2025-04-08 19:42:39 -0400

    BlueLeftAlgae
    Doesn't work yet but my sim isn't working so I'm committing so we can work on someone elses computer.

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -5,13 +5,13 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "2025RobotCode";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 536;
-  public static final String GIT_SHA = "e4dc91b24d1ad0c42c6ba0d89b84009d5bb369f9";
-  public static final String GIT_DATE = "2025-04-03 22:40:27 EDT";
-  public static final String GIT_BRANCH = "msc";
-  public static final String BUILD_DATE = "2025-04-04 14:50:28 EDT";
-  public static final long BUILD_UNIX_TIME = 1743792628322L;
-  public static final int DIRTY = 1;
+  public static final int GIT_REVISION = 538;
+  public static final String GIT_SHA = "ab44bce17ccf313553ca6c3a179331c4268a9500";
+  public static final String GIT_DATE = "2025-04-08 17:51:37 EDT";
+  public static final String GIT_BRANCH = "SideAutoThreeCoralPlusAlgaeOff";
+  public static final String BUILD_DATE = "2025-04-08 19:41:44 EDT";
+  public static final long BUILD_UNIX_TIME = 1744155704478L;
+  public static final int DIRTY = 128;
 
   private BuildConstants() {}
 }
