7b0724fb55e3c4d5bb81e8d3d7a6342f6399cd47
ArnavGuptaOfficial <arnavgupta045@gmail.com>
2025-04-08 21:15:32 -0400

    Completed Barge Algae
    Completed Barge algae, don't know if its completely working because my sim was glitching but it should

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -5,12 +5,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "2025RobotCode";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 539;
-  public static final String GIT_SHA = "76164bc2dc227a63d895b8c2ae9a9b6da1b33587";
-  public static final String GIT_DATE = "2025-04-08 19:42:39 EDT";
+  public static final int GIT_REVISION = 540;
+  public static final String GIT_SHA = "4f4055833c75c4c29856b8d91db7c0de67b9f9d0";
+  public static final String GIT_DATE = "2025-04-08 20:36:44 EDT";
   public static final String GIT_BRANCH = "SideAutoThreeCoralPlusAlgaeOff";
-  public static final String BUILD_DATE = "2025-04-08 20:35:43 EDT";
-  public static final long BUILD_UNIX_TIME = 1744158943474L;
+  public static final String BUILD_DATE = "2025-04-08 21:14:44 EDT";
+  public static final long BUILD_UNIX_TIME = 1744161284489L;
   public static final int DIRTY = 128;
 
   private BuildConstants() {}
