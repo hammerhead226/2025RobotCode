9b5c97b96f0edc763d3f939488918cbc3aadfa31
David Chen <dychen2523@gmail.com>
2025-04-18 13:28:11 -0400

    changed auto align offset to move left

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -5,12 +5,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "2025RobotCode";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 549;
-  public static final String GIT_SHA = "c8b4d74f38803d3c8b41e1569f54ed8e248578cb";
-  public static final String GIT_DATE = "2025-04-17 15:55:40 EDT";
+  public static final int GIT_REVISION = 550;
+  public static final String GIT_SHA = "7591bf4c39cfef022d5ebab4936204cdf816a29f";
+  public static final String GIT_DATE = "2025-04-18 08:57:24 EDT";
   public static final String GIT_BRANCH = "cmp";
-  public static final String BUILD_DATE = "2025-04-18 08:54:23 EDT";
-  public static final long BUILD_UNIX_TIME = 1744980863067L;
+  public static final String BUILD_DATE = "2025-04-18 13:21:15 EDT";
+  public static final long BUILD_UNIX_TIME = 1744996875083L;
   public static final int DIRTY = 1;
 
   private BuildConstants() {}
