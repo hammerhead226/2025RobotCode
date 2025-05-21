76d4dd34fff2f08e0a010975365363693bc83043
devinisaperson <177495008+devinisaperson@users.noreply.github.com>
2025-05-20 20:09:25 -0400

    documented driver and manip controls

--- src/main/java/frc/robot/commands/ReinitializingCommand.java
+++ src/main/java/frc/robot/commands/ReinitializingCommand.java
@@ -11,7 +11,7 @@ import java.util.function.Supplier;
 /* You should consider using the more terse Command factories API instead https://docs.wpilib.org/en/stable/docs/software/commandbased/organizing-command-based.html#defining-commands */
 /**
  * Command that on start up gets a command from a supplier and then runs it (effectively a wrapper).
- * Given to triggers so that they do not get locked to one command on inital evalutation. 
+ * Given to triggers so that they do not get locked to one command on initial evaluation. 
  * @author Devin Huang
  */
 public class ReinitializingCommand extends Command {
@@ -19,7 +19,7 @@ public class ReinitializingCommand extends Command {
   Command command;
 
   /**
-   * @param commandSupplier the supplier which returns a command to be run each time this command is reinitalized
+   * @param commandSupplier the supplier which returns a command to be run each time this command is reinitialized
    * @param requirements optional parameters to specify any number of subsystems required
    */
   public ReinitializingCommand(Supplier<Command> commandSupplier, Subsystem... requirements) {
