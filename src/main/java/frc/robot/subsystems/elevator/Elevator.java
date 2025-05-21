cfa78c6398bffd1b196da4c32cd840a416d0973b
devinisaperson <177495008+devinisaperson@users.noreply.github.com>
2025-05-20 20:40:57 -0400

    edit

--- src/main/java/frc/robot/subsystems/elevator/Elevator.java
+++ src/main/java/frc/robot/subsystems/elevator/Elevator.java
@@ -166,7 +166,7 @@ public class Elevator extends SubsystemBase {
    * used in SuperStructure's shouldSlowMode()
    * which is read by RobotContainer to create a trigger
    * and that trigger is used exclusively to mirror the slow mode boolean in Drive
-   * finally Drive's isSlowMode() is used in AdjustToReef
+   * finally Drive's isSlowMode() is used in AdjustToReef and DriveCommands
    * 
    * @return whether the robot should be in "slow mode"
    */
