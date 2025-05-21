c8a86d400fcb4b57decd5218026d078f1c854a3e
devinisaperson <177495008+devinisaperson@users.noreply.github.com>
2025-05-20 20:39:48 -0400

    TODO fix slowmode

--- src/main/java/frc/robot/subsystems/elevator/Elevator.java
+++ src/main/java/frc/robot/subsystems/elevator/Elevator.java
@@ -159,6 +159,17 @@ public class Elevator extends SubsystemBase {
     extenderProfile = new TrapezoidProfile(extenderConstraints);
   }
 
+  // TODO fix the fact this information is pipped through so many different classes
+  /**
+   * determines if the robot should drive slow to avoid tipping due to high center of mass
+   * 
+   * used in SuperStructure's shouldSlowMode()
+   * which is read by RobotContainer to create a trigger
+   * and that trigger is used exclusively to mirror the slow mode boolean in Drive
+   * finally Drive's isSlowMode() is used in AdjustToReef
+   * 
+   * @return whether the robot should be in "slow mode"
+   */
   public boolean shouldSlowMode() {
     return extenderGoal.position >= 10;
   }
