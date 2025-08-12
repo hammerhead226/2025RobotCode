3b64718bfcf2fa5bf4b717544073fcc639865694
devinisaperson <177495008+devinisaperson@users.noreply.github.com>
2025-08-12 19:45:44 -0400

    fixed elastic

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -5,12 +5,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "2025RobotCode";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 555;
-  public static final String GIT_SHA = "48e056879baf78f004d2df4f2459f667293d3be8";
-  public static final String GIT_DATE = "2025-08-05 23:34:29 EDT";
+  public static final int GIT_REVISION = 556;
+  public static final String GIT_SHA = "ffb3df0023d092665e5ec95f8351974ed6744ee2";
+  public static final String GIT_DATE = "2025-08-12 00:23:19 EDT";
   public static final String GIT_BRANCH = "localClimb";
-  public static final String BUILD_DATE = "2025-08-12 00:19:06 EDT";
-  public static final long BUILD_UNIX_TIME = 1754972346987L;
+  public static final String BUILD_DATE = "2025-08-12 19:25:09 EDT";
+  public static final long BUILD_UNIX_TIME = 1755041109325L;
   public static final int DIRTY = 128;
 
   private BuildConstants() {}
