90026c2b10dc69caf0fd95b4d52b0711a6ba24ed
zachashaw <90483131+zachashaw@users.noreply.github.com>
2025-08-13 20:52:35 -0400

    auto adjustments
    1 sec wait -> 0.2 sec
    processor: -0.1m +0.1m to source pos

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -5,12 +5,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "2025RobotCode";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 557;
-  public static final String GIT_SHA = "3b64718bfcf2fa5bf4b717544073fcc639865694";
-  public static final String GIT_DATE = "2025-08-12 19:45:44 EDT";
-  public static final String GIT_BRANCH = "localClimb";
-  public static final String BUILD_DATE = "2025-08-12 20:03:27 EDT";
-  public static final long BUILD_UNIX_TIME = 1755043407517L;
+  public static final int GIT_REVISION = 559;
+  public static final String GIT_SHA = "6cabab1ef3463f26b854cb3f9dc1e2a503834d04";
+  public static final String GIT_DATE = "2025-08-13 18:39:07 EDT";
+  public static final String GIT_BRANCH = "main";
+  public static final String BUILD_DATE = "2025-08-13 19:48:31 EDT";
+  public static final long BUILD_UNIX_TIME = 1755128911951L;
   public static final int DIRTY = 128;
 
   private BuildConstants() {}
