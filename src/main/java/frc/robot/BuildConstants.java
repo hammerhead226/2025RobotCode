8463307278e411664f457573b18346405fe3af3a
zachashaw <90483131+zachashaw@users.noreply.github.com>
2025-08-16 13:47:50 -0400

    add withtimeout to auto
    full send pre match 8

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -5,12 +5,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "2025RobotCode";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 563;
-  public static final String GIT_SHA = "6f094732f2ca80078af96cca74bb240785422fea";
-  public static final String GIT_DATE = "2025-08-16 08:34:18 EDT";
+  public static final int GIT_REVISION = 564;
+  public static final String GIT_SHA = "d2deedbdae2607f81ec3d2220b8566ce55a2245d";
+  public static final String GIT_DATE = "2025-08-16 13:05:23 EDT";
   public static final String GIT_BRANCH = "MARC-2025";
-  public static final String BUILD_DATE = "2025-08-16 12:13:45 EDT";
-  public static final long BUILD_UNIX_TIME = 1755360825402L;
+  public static final String BUILD_DATE = "2025-08-16 13:05:52 EDT";
+  public static final long BUILD_UNIX_TIME = 1755363952392L;
   public static final int DIRTY = 128;
 
   private BuildConstants() {}
--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -351,7 +351,7 @@ public class RobotContainer {
         "SEE_CORAL",
         new SequentialCommandGroup(
             // new WaitCommand(1.5),
-            new WaitUntilCommand(() -> scoralRollers.seesCoral() == CoralState.SENSOR),
+            new WaitUntilCommand(() -> scoralRollers.seesCoral() == CoralState.SENSOR).withTimeout(1.3),
             new InstantCommand(() -> scoralRollers.stop())));
 
     NamedCommands.registerCommand(
